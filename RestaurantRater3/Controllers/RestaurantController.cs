d8bc9af74c7ec07b0e28d3496602841b3e1c2fda
r3evans <r3evans@gmail.com>
2020-06-22 16:27:27 -0400

    first commit

---
+++ RestaurantRater3/Controllers/RestaurantController.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantRater3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater3.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(_db.Restaurants.ToList());
+        }
+    }
+}
